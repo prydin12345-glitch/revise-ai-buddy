cd9c0c2de6a627159f0f9be6e783c7463609e3e0
prydin12345-glitch <prydin12345@gmail.com>
2026-07-18 16:52:12 +0100

    Update SubjectsList.tsx

--- src/components/stats/SubjectsList.tsx
+++ src/components/stats/SubjectsList.tsx
@@ -73,7 +73,7 @@ export const SubjectsList = () => {
         </Button>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-8">
         {filtered.map((subject) => (
           <SubjectCard
             key={subject.id}
